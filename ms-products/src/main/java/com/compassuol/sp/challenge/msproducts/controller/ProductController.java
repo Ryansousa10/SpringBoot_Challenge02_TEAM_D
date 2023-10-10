ae1a81ba9ceb476f6c9a7aa326f3afd41d083d58
ryansousa10 <ryan.sousa4000@gmail.com>
2023-10-10 20:33:14 -0300

    Implementation of the GET endpoint that returns all products

--- ms-products/src/main/java/com/compassuol/sp/challenge/msproducts/controller/ProductController.java
+++ ms-products/src/main/java/com/compassuol/sp/challenge/msproducts/controller/ProductController.java
@@ -1,4 +1,23 @@
 package com.compassuol.sp.challenge.msproducts.controller;
 
+import com.compassuol.sp.challenge.msproducts.model.ProductModel;
+import com.compassuol.sp.challenge.msproducts.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/products")
 public class ProductController {
+
+    @Autowired
+    private ProductService productService;
+
+    @GetMapping
+    public List<ProductModel> getProducts() {
+        return productService.getAllProducts();
+    }
 }
--- ms-products/src/main/java/com/compassuol/sp/challenge/msproducts/service/ProductService.java
+++ ms-products/src/main/java/com/compassuol/sp/challenge/msproducts/service/ProductService.java
@@ -1,4 +1,19 @@
 package com.compassuol.sp.challenge.msproducts.service;
 
+import com.compassuol.sp.challenge.msproducts.model.ProductModel;
+import com.compassuol.sp.challenge.msproducts.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class ProductService {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    public List<ProductModel> getAllProducts() {
+        return productRepository.findAll();
+    }
 }
