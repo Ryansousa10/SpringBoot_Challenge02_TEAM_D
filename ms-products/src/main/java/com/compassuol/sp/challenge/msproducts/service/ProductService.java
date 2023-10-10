4fa3c19abd9eccda44907a2dc9b98d8cefed2866
rian <rianb@CENTRAL-DESK.(none)>
2023-10-10 16:38:32 -0300

    already for pull

---
+++ ms-products/src/main/java/com/compassuol/sp/challenge/msproducts/service/ProductService.java
@@ -0,0 +1,4 @@
+package com.compassuol.sp.challenge.msproducts.service;
+
+public class ProductService {
+}
--- ms-products/src/main/resources/db/migration/V3__Create_table_products.sql
+++ ms-products/src/main/resources/db/migration/V3__Create_table_products.sql
@@ -1,6 +1,6 @@
 CREATE TABLE `products_tb` (
   `id` INT AUTO_INCREMENT PRIMARY KEY,
-  `name` VARCHAR(255) NOT NULL UNIQUE,
+  `name` VARCHAR(255),
   `description` VARCHAR(255),
   `price` DOUBLE
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
