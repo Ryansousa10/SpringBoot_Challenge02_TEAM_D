7da66b98242fbd7e21d45eb37ebd5cd47fb344cb
rian <rianb@CENTRAL-DESK.(none)>
2023-10-10 20:13:19 -0300

    entity added

--- ms-products/src/main/java/com/compassuol/sp/challenge/msproducts/repository/ProductRepository.java
+++ ms-products/src/main/java/com/compassuol/sp/challenge/msproducts/repository/ProductRepository.java
@@ -1,4 +1,9 @@
 package com.compassuol.sp.challenge.msproducts.repository;
 
-public class ProductRepository {
+import com.compassuol.sp.challenge.msproducts.model.ProductModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<ProductModel, Long> {
 }
