cfc308e8b7feba7d9f55b44c70a01f3dafdbfb76
ryansousa10 <ryan.sousa4000@gmail.com>
2023-10-11 19:00:25 -0300

    ProductModel entity now has 100% test coverage

--- ms-products/src/main/java/com/compassuol/sp/challenge/msproducts/model/ProductModel.java
+++ ms-products/src/main/java/com/compassuol/sp/challenge/msproducts/model/ProductModel.java
@@ -3,9 +3,10 @@ package com.compassuol.sp.challenge.msproducts.model;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.Size;
-import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
-@Data
+
+@Getter
 @Entity
 @Table(name = "products_tb")
 @NoArgsConstructor
@@ -25,38 +26,6 @@ public class ProductModel {
     public ProductModel(String name, String description, Double value) {
         this.name = name;
         this.description = description;
-        this.value = value;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public Double getValue() {
-        return value;
-    }
-
-    public void setValue(Double value) {
         this.value = value;
     }
 }
