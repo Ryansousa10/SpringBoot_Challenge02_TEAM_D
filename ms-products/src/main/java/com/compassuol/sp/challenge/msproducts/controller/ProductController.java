6a27ee0f5458a2d1e2b1dfdb93569fee3d90eb67
Djoseffer <tom_djosefer@hotmail.com>
2023-10-10 23:32:58 -0300

    initial method get products by id

--- ms-products/src/main/java/com/compassuol/sp/challenge/msproducts/controller/ProductController.java
+++ ms-products/src/main/java/com/compassuol/sp/challenge/msproducts/controller/ProductController.java
@@ -1,4 +1,24 @@
 package com.compassuol.sp.challenge.msproducts.controller;
 
+import com.compassuol.sp.challenge.msproducts.model.ProductModel;
+import com.compassuol.sp.challenge.msproducts.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/products")
 public class ProductController {
+    private final ProductRepository productRepository;
+
+    @Autowired
+    public ProductController(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    @GetMapping("/{productId}")
+    public Optional<ProductModel> getProductById(@PathVariable int productId) {
+        return productRepository.findById(productId);
+    }
 }
--- ms-products/src/main/java/com/compassuol/sp/challenge/msproducts/model/ProductModel.java
+++ ms-products/src/main/java/com/compassuol/sp/challenge/msproducts/model/ProductModel.java
@@ -1,4 +1,17 @@
 package com.compassuol.sp.challenge.msproducts.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class ProductModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+    private String description;
+    private double price;
+
 }
